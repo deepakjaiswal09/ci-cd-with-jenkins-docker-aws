f2a11ffc2ea2652e8ce624b62ef97487195af621
deepakjwll <deepakjaiswal9238@gmail.com>
2025-09-27 17:12:40 +0530

    commit the code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from the Advanced Jenkins CI/CD Pipeline!\n");
+  res.end("Hello from the Advanced Jenkins CI/CD Pipeline! v2\n");
 });
 
 server.listen(port, () => {
